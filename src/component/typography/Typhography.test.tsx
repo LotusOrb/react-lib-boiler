fc65a318dcae496e96c74b45f6bf4140366181c3
LotusOrb <rsyafridamara@gmail.com>
2022-04-09 22:30:37 +0700

    fix : eslint warning

--- src/component/typography/Typhography.test.tsx
+++ src/component/typography/Typhography.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { fireEvent, render } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { Typography } from "./Typhography";
 import { act } from "react-dom/test-utils";
 
